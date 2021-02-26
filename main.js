c824b6ffd7d8e5c847d3046125ea4d3bd39018c3
tashadezelsky <77847692+tashadezelsky@users.noreply.github.com>
2021-02-26 23:08:00 +0900

    more changes + javascript

--- main.js
+++ main.js
@@ -6,8 +6,19 @@ window.onload = () => {
        transition_el.classList.remove('is-active');
    }, 500);
 
-   for (let index= 0; index < anchors.length; index++) {
-       const anchor = anchors[index];
+   for (let i = 0; i < anchors.length; i++) {
+       const anchor = anchors[i];
+
+       anchor.addEventListener('click', e => {
+           e.preventDefault();
+           let target = e.target.href;
+
+           transition_el.classList.add('is-active')
+       }) 
+
+       setTimeout(() => {
+           window.location.href = target;
+       }, 500);
        
    }
 } 
