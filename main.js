fdbbd7ca244f393090e60c9697b66fe54b079efa
tashadezelsky <77847692+tashadezelsky@users.noreply.github.com>
2021-03-02 21:53:39 +0900

    project 2 final changes

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 window.onload = () => {
     const transition_el = document.querySelector('.transition');
-    const anchors = document.querySelectorAll('a');
+    const anchors = document.querySelectorAll('page2.html');
 
    setTimeout(() => {
        transition_el.classList.remove('is-active');
    }, 500);
 
    for (let i = 0; i < anchors.length; i++) {
-       const anchor = anchors[i];
+        const anchor = anchors[i];
 
        anchor.addEventListener('click', e => {
            e.preventDefault();
            let target = e.target.href;
 
-           transition_el.classList.add('is-active')
-       }) 
+           transition_el.classList.add('is-active');
 
-       setTimeout(() => {
-           window.location.href = target;
-       }, 500);
-       
-   }
-} 
+           setTimeout(() => {
+               window.location.href = target;
+           }, 500);
+
+       });
+    }
+
+    } 
+    
